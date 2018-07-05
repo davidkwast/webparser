d4adbd1fd85528bca941768447acf860e509c9b1
David Kwast <david@kwast.me>
2018-07-05 11:38:56 -0300

    Youtube search API: next_page_link fix

--- webparser/youtube/api.py
+++ webparser/youtube/api.py
@@ -1,8 +1,10 @@
+from urllib.parse import urljoin
+
 import webparser.api
 
 def search(query):
     COOKIES = {'PREF': 'f1=50000000&gl=BR&hl=en'}
-    URL = 'https://www.youtube.com/results?locale=en_US&search_query='
+    URL = 'https://www.youtube.com/results?search_query='
     SPLIT = '/watch?v='
     response = webparser.api.request(URL + query, cookies=COOKIES)
     html = response.html
@@ -10,7 +12,8 @@ def search(query):
     videos = [x.split(SPLIT)[-1] for x in links if SPLIT in x]
     next_page_link__list = [x for x in html.find('span.yt-uix-button-content') if x.text == 'Next »']
     if next_page_link__list:
-        next_page_link = html.base_url + next_page_link__list[0].element.getparent().attrib['href'][1:]
+        link = next_page_link__list[0].element.getparent().attrib['href']
+        next_page_link = urljoin(response.url, link)
     else:
         next_page_link = None
     return {
